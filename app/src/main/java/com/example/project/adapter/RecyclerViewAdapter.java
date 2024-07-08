3fe6efd166994ea6e61fb63e47ed67e3ef67d802
aceto <tienhmhe130651@fpt.edu.vn>
2024-07-08 21:16:44 +0700

    update todayviewadapter and fragment today

--- app/src/main/java/com/example/project/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/project/adapter/RecyclerViewAdapter.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,14 +14,21 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.project.R;
 import com.example.project.model.Task;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.TodayViewHolder> {
     private List<Task> list;
+    private TaskListenr taskListenr;
 
     public RecyclerViewAdapter() {
         list = new ArrayList<>();
+    }
+
+    public void setTaskListenr(TaskListenr taskListenr) {
+        this.taskListenr = taskListenr;
     }
 
     public void setList(List<Task> list){
@@ -35,11 +43,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @Override
     public TodayViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.task,parent,false);
-        return null;
+        return new TodayViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull TodayViewHolder holder, int position) {
+        Task task = list.get(position);
+        holder.title.setText(task.getTitle());
+        holder.desc.setText(task.getDescription());
+        Date duedate = task.getDueDate();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String dateString = dateFormat.format(duedate);
+        holder.date.setText(dateString);
 
     }
 
@@ -48,21 +63,34 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return list.size();
     }
 
-    public class TodayViewHolder extends RecyclerView.ViewHolder{
+    public class TodayViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private TextView title,desc,date,category;
-        private Button edit;
-        private CheckBox check;
+        private CheckBox cbIsCheck;
 
-        public TodayViewHolder(@NonNull View itemView) {
-            super(itemView);
+        private void bindingView() {
             title = itemView.findViewById(R.id.todoTitle);
             desc = itemView.findViewById(R.id.todoDescription);
             date = itemView.findViewById(R.id.todoDateTime);
             category = itemView.findViewById(R.id.todoCategory);
-            edit = itemView.findViewById(R.id.todoBtnEdit);
-            check = itemView.findViewById(R.id.todoCheckBox);
+            cbIsCheck = itemView.findViewById(R.id.todoCheckBox);
+            itemView.setOnClickListener(this);
+        }
+
+        public TodayViewHolder(@NonNull View itemView) {
+            super(itemView);
+            bindingView();
 
 
         }
+
+        @Override
+        public void onClick(View view) {
+            if(taskListenr!=null){
+                taskListenr.onTaskClick(view,getAdapterPosition());
+            }
+        }
+    }
+    public interface TaskListenr{
+        void onTaskClick(View view, int position);
     }
 }
