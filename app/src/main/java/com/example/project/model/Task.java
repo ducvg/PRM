2ddec9f0bc049756a35a2275ca5969b9d579be5f
aceto <tienhmhe130651@fpt.edu.vn>
2024-07-10 06:58:25 +0700

    update today

--- app/src/main/java/com/example/project/model/Task.java
+++ app/src/main/java/com/example/project/model/Task.java
@@ -1,23 +1,125 @@
 package com.example.project.model;
 
+
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Task implements Serializable {
     private int taskId;
-    private int userId;
-    private String title;
-    private String description;
-    private Date dueDate;
-    private Date createdAt;
-    private Date updatedAt;
-    private int status;
-    private int categoryId;
-    private Category category;
-    private User user;
-    private Set<Attachment> attachments = new HashSet<>();
-    private Set<Subtask> subtasks = new HashSet<>();
 
+    private int userId;
+
+    private String title;
+
+    private String description;
+
+    private Date dueDate;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private int status;
+
+    private int categoryId;
+
+    public Task() {
+    }
+
+    public Task(int taskId, String title, String description, Date dueDate, int status, int categoryId) {
+        this.taskId = taskId;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.categoryId = categoryId;
+    }
+
+
+    public Task(int taskId, int userId, String title, String description, Date dueDate, LocalDateTime createdAt, LocalDateTime updatedAt, int status, int categoryId) {
+        this.taskId = taskId;
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.status = status;
+        this.categoryId = categoryId;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
 }
